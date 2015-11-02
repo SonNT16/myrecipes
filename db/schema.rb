312d75c5f0608ebfd8870fceb387995f0e371608
Ngo Thanh Son <ngothanhson88dn@gmail.com>
2015-11-02 16:42:35 +0700

    create chefs and recipes association and tests

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151101162824) do
+ActiveRecord::Schema.define(version: 20151102091819) do
 
   create_table "articles", force: true do |t|
     t.string   "title"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 20151101162824) do
     t.text     "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "chef_id"
   end
 
   create_table "users", force: true do |t|
