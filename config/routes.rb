e3a9107748fd53ebb7c5461cfde56174917e6ee9
Ngo Thanh Son <ngothanhson88dn@gmail.com>
2015-11-03 17:48:59 +0700

    create

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  
-  resources :articles do
-    resources :comments
-  end
 
   root 'pages#home'
-  #get '/home', to: 'pages#home'
+  get '/home', to: 'pages#home'
+  get '/recipes', to: 'recipes#index'
+  get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
+  #post '/recipes', to: 'recipes#create'
+  get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+  patch 'recipes/:id', to: 'recipes#update'
+  get 'recipes/:id', to: 'recipes#show', as: 'recipe'
+  #delete '/recipes/:id', to: 'recipes#destroy'
 
-
- 
+  resource :recipes
 end
