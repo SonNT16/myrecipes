d7a7dd6ee1b9184bdd8d624918b3a6d26952938e
Ngo Thanh Son <ngothanhson88dn@gmail.com>
2015-11-10 23:16:02 +0700

    create chef

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,11 +1,14 @@
 class RecipesController < ApplicationController
 
+  before_action :set_recipe, only: [:edit, :update, :show, :like]
+  before_action :require_user, except: [:show, :index]
+  before_action :require_same_user, only: [:edit, :update]
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
   end
 
   def show
-    @recipe = Recipe.find(params[:id])
+
   end
 
   def new
@@ -14,7 +17,7 @@ class RecipesController < ApplicationController
 
   def create
     @recipe = Recipe.new(recipe_params)
-    @recipe.chef = Chef.find(2)
+    @recipe.chef = current_user
     if @recipe.save
       flash[:success] = "Your recipe was created successfully!"
       redirect_to recipes_path
@@ -24,12 +27,10 @@ class RecipesController < ApplicationController
   end
 
   def edit
-    @recipe = Recipe.find(params[:id])
   end
 
   def like
-    @recipe = Recipe.find(params[:id])
-    like = Like.create(like: params[:like],chef: Chef.first,recipe: @recipe)
+    like = Like.create(like: params[:like],chef: current_user,recipe: @recipe)
     if like.valid?
       flash[:success] = "Your selection was successful"
       redirect_to :back
@@ -40,7 +41,6 @@ class RecipesController < ApplicationController
 
   end
   def update
-    @recipe = Recipe.find(params[:id])
     if @recipe.update(recipe_params)
       flash[:success] = "Your recipe was updated successfully!"
       redirect_to recipe_path(@recipe)
@@ -49,6 +49,17 @@ class RecipesController < ApplicationController
     end
   end
   private
+
+    def set_recipe
+      @recipe = Recipe.find(params[:id])
+    end
+
+    def require_same_user
+      if @recipe.chef != current_user
+        flash[:danger] = "You can only edit your own recipes"
+        redirect_to recipes_path
+      end
+    end
     def recipe_params
       params.require(:recipe).permit(:name, :summary, :description, :picture)
     end
