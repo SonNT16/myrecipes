06d4f04437f0b60229d34327a1b2dbd3c6f0f7e5
Ngo Thanh Son <ngothanhson88dn@gmail.com>
2015-11-15 14:06:59 +0700

    update

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,7 +1,8 @@
 class RecipesController < ApplicationController
 
   before_action :set_recipe, only: [:edit, :update, :show, :like]
-  before_action :require_user, except: [:show, :index]
+  before_action :require_user, except: [:show, :index, :like]
+  before_action :require_user_like, only: [:like]
   before_action :require_same_user, only: [:edit, :update]
   def index
     @recipes = Recipe.paginate(page: params[:page], per_page: 4)
@@ -61,6 +62,12 @@ class RecipesController < ApplicationController
       end
     end
     def recipe_params
-      params.require(:recipe).permit(:name, :summary, :description, :picture)
+      params.require(:recipe).permit(:name, :summary, :description, :picture, style_ids: [], ingredient_ids: [])
+    end
+    def require_user_like
+      if !logged_in?
+        flash[:danger] = "You must be logged in to perform that action"
+        redirect_to :back
+      end
     end
 end
